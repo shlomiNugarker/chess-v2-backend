f16991de8bc28658e9f70daaebfd50758eda91fd
Shlomi Nugarker <shlomin1231@gmail.com>
2022-11-10 20:03:04 +0200

    more files

---
+++ config/prod.ts
@@ -0,0 +1,3 @@
+module.exports = {
+  dbURL: `mongodb+srv://shlomin1231:${process.env.DB_CHESS}@cluster0.ysm5t.mongodb.net/social_network_db?retryWrites=true&w=majority`,
+}
---
+++ services/dbService.ts
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient
+
+const config = require('../config')
+
+module.exports = {
+  getCollection,
+}
+
+const dbName = 'chess_db'
+
+var dbConn: any = null
+
+async function getCollection(collectionName: string) {
+  try {
+    const db = await connect()
+    const collection = await db.collection(collectionName)
+    return collection
+  } catch (err) {
+    console.log('Failed to get Mongo collection', err)
+    throw err
+  }
+}
+
+async function connect() {
+  if (dbConn) return dbConn
+  try {
+    const client = await MongoClient.connect(config.dbURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    const db = client.db(dbName)
+    dbConn = db
+    return db
+  } catch (err) {
+    console.log('Cannot Connect to DB', err)
+    throw err
+  }
+}
