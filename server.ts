0ec307f6f90b0c912440d9075e1d06405b5f21de
Shlomi Nugarker <shlomin1231@gmail.com>
2022-11-13 13:55:06 +0200

    build server.ts

--- server.ts
+++ server.ts
@@ -22,10 +22,10 @@ const session = expressSession({
 
 app.use(session)
 app.use(json())
-app.use(express.static('../public'))
+app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../public')))
+  app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
   const corsOptions = {
     origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
